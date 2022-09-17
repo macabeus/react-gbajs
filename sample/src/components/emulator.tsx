3ba846ed2f103469610921482f01ec0a5a8e78ba
Eddie Wang <eddiewang12345@gmail.com>
2022-09-17 02:17:48 -0400

    fix FlashSavedata not defined, hex not defined

--- sample/src/components/emulator.tsx
+++ sample/src/components/emulator.tsx
@@ -1,23 +1,25 @@
-import React, { FunctionComponent } from 'react'
-import ReactGbaJs from 'react-gbajs'
+import React, { FunctionComponent } from 'react';
+import ReactGbaJs from 'react-gbajs';
 
-type Props = { scale: number, printFps: boolean }
+type Props = { scale: number; printFps: boolean };
 const Emulator: FunctionComponent<Props> = ({ scale, printFps }) => {
-  const fpsReportCallback = (
-    printFps
-      ? (fps: number) => console.log('FPS', fps)
-      : undefined
-  )
+  const fpsReportCallback = printFps
+    ? (fps: number) => console.log('FPS', fps)
+    : undefined;
 
   return (
     <ReactGbaJs
       onFpsReported={fpsReportCallback}
       scale={scale}
-      volume={0}
+      volume={1}
       watchLogLevels={{ error: true, warn: true }}
-      onLogReceived={(level, message) => { console.log(`Received a log level ${level} with the message "${message}"`) }}
+      onLogReceived={(level, message) => {
+        console.log(
+          `Received a log level ${level} with the message "${message}"`
+        );
+      }}
     />
-  )
-}
+  );
+};
 
-export default Emulator
+export default Emulator;
